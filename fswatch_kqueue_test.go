bd6eef43f082eea764f01fe4dad610bfcbdba0f0
Dave Cheney <dave@cheney.net>
2012-12-29 17:58:15 +1100

    more tests

--- fswatch_kqueue_test.go
+++ fswatch_kqueue_test.go
@@ -7,8 +7,24 @@
 package fswatch
 
 import (
+	"syscall"
 	"testing"
 )
+
+func checkev(t *testing.T, expected, actual *syscall.Kevent_t) {
+	if expected.Ident != actual.Ident {
+		t.Errorf("ev.Ident: expected %d, received: %d", expected.Ident, actual.Ident)
+	}
+	if expected.Filter != actual.Filter {
+		t.Errorf("ev.Filter: expected %d, received: %d", expected.Filter, actual.Filter)
+	}
+	if expected.Flags != actual.Flags {
+		t.Errorf("ev.Flags: expected %d, received: %d", expected.Flags, actual.Flags)
+	}
+	if expected.Fflags != actual.Fflags {
+		t.Errorf("ev.Fflags: expected %d, received: %d", expected.Fflags, actual.Fflags)
+	}
+}
 
 func TestKqueuenewWatcher(t *testing.T) {
 	w, err := newWatcher()
@@ -28,4 +44,27 @@ func TestKqueuewatcheradd(t *testing.T) {
 	defer f.Close()
 
 	w.add(f.Name())
+}
+
+func TestKqueuewait(t *testing.T) {
+	d, done := setup(t)
+	defer done()
+
+	w, err := newWatcher()
+	assert(t, err)
+	defer w.close()
+
+	f := newfile(t, d)
+	defer f.Close()
+
+	w.add(f.Name())
+
+	n, err := f.Write([]byte("hey hey"))
+	assert(t, err)
+	var buf [1]syscall.Kevent_t
+	n, err = w.kqueue.wait(buf[:])
+	assert(t, err)
+	if n != len(buf) {
+		t.Fatalf("expecting 1 notification, received %d", n)
+	}
 }
