09e2b5e4eb05557601fba27e4836b23c547fbf75
Dave Cheney <dave@cheney.net>
2012-12-30 17:27:24 +1100

    added run method

--- fswatch_kqueue.go
+++ fswatch_kqueue.go
@@ -8,6 +8,7 @@ package fswatch
 
 import (
 	"os"
+	"sync"
 	"syscall"
 )
 
@@ -15,6 +16,9 @@ type watcher struct {
 	*kqueue
 	C     chan *Event
 	waker *os.File
+	sync.RWMutex
+	fds   map[string]int
+	paths map[int]string
 }
 
 func newWatcher() (*watcher, error) {
@@ -36,6 +40,8 @@ func newWatcher() (*watcher, error) {
 		kqueue: kq,
 		C:      make(chan *Event, 1),
 		waker:  pw,
+		fds:    make(map[string]int),
+		paths:  make(map[int]string),
 	}, nil
 }
 
@@ -44,20 +50,45 @@ func (w *watcher) add(path string) error {
 	if err != nil {
 		return os.NewSyscallError("open", err)
 	}
+	w.Lock()
+	defer w.Unlock()
+	if _, exists := w.fds[path]; exists {
+		defer syscall.Close(fd)
+		return ErrWatchExists
+	}
+	w.fds[path] = fd
+	w.paths[fd] = path
 	return w.kqueue.add(uintptr(fd), syscall.EVFILT_VNODE, 0, syscall.NOTE_DELETE|syscall.NOTE_EXTEND|syscall.NOTE_WRITE)
 }
 
 func (w *watcher) run() {
-	// nothing
+	defer w.kqueue.close()
+	var buf [18]syscall.Kevent_t
+	for done := false; !done; {
+		n, err := w.kqueue.wait(buf[:])
+		if err != nil {
+			println(err.Error())
+			return
+		}
+		for _, e := range buf[:n] {
+			if uintptr(e.Ident) == w.waker.Fd() {
+				done = true
+				continue
+			}
+			w.C <- &Event{
+				Target: w.paths[int(e.Ident)],
+				event:  event(e),
+			}
+		}
+	}
+	println("done")
 }
 
 func (w *watcher) close() error {
 	return w.waker.Close()
 }
 
-type event struct {
-	mask uint32
-}
+type event syscall.Kevent_t
 
-func (e *event) IsCreate() bool { return e.mask&syscall.NOTE_EXTEND > 0 }
-func (e *event) IsRemove() bool { return e.mask&syscall.NOTE_DELETE > 0 }
+func (e *event) IsCreate() bool { return e.Flags&syscall.NOTE_EXTEND > 0 }
+func (e *event) IsRemove() bool { return e.Flags&syscall.NOTE_DELETE > 0 }
